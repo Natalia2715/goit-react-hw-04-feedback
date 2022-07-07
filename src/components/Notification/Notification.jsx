5e8c3742ba2c9e7b74d6baf155a58ef1b3643192
Natalia2715 <92229205+Natalia2715@users.noreply.github.com>
2022-07-07 10:06:56 +0300

    fix

--- src/components/Notification/Notification.jsx
+++ src/components/Notification/Notification.jsx
@@ -1,10 +1,12 @@
 import PropTypes from 'prop-types';
 import styles from './Notification.module.css';
 
-export default Notification = ({ message }) => (
-  <p className={styles.notification}>{message}</p>
-);
+function Notification({ message }) {
+  return <p className={styles.notification}>{message}</p>;
+}
 
 Notification.propTypes = {
-    message: PropTypes.string,
-}
+  message: PropTypes.string,
+};
+
+export default Notification;
