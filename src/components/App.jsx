279b9b560530b7ebddd871246da39a16db603133
Natalia2715 <92229205+Natalia2715@users.noreply.github.com>
2022-07-17 12:28:24 +0300

    hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,52 +1,69 @@
-import React, { Component } from 'react';
+import { useState } from 'react';
 import Section from './Section/Section';
 
 import FeedbackOptions from './FeedbackOptions/FeedbackOptions';
 import Statistics from './Statistics/Statistics';
 import Notification from './Notification/Notification';
 
-export default class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  };
+export default function App() {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
-  onLeaveFeedback = event => {
+  // state = {
+  //   good: 0,
+  //   neutral: 0,
+  //   bad: 0,
+  // };
+
+  const onLeaveFeedback = event => {
     const item = event.currentTarget.textContent;
-    this.setState(prevState => {
-      return { [item]: prevState[item] + 1 };
-    });
+
+    switch (item) {
+      case 'good':
+        setGood(prevState => prevState + 1);
+        break;
+      case 'neutral':
+        setNeutral(prevState => prevState + 1);
+        break;
+      case 'bad':
+        setBad(prevState => prevState + 1);
+        break;
+      default:
+        return;
+    }
   };
 
-  countTotalFeedback() {
-    return this.state.good + this.state.neutral + this.state.bad;
-  }
+  const countTotalFeedback = () => {
+    return good + neutral + bad;
+  };
 
-  countPositiveFeedbackPercentage() {
-    return Math.round((this.state.good * 100) / this.countTotalFeedback());
-  }
+  const countPositiveFeedbackPercentage = () => {
+    return Math.round((good * 100) / countTotalFeedback());
+  };
 
-  render() {
-    const options = Object.keys(this.state);
-    return (
-      <Section title="Please leave feedback">
-        <FeedbackOptions
-          options={options}
-          onLeaveFeedback={this.onLeaveFeedback}
-        ></FeedbackOptions>
-        {this.countTotalFeedback() > 0 ? (
-          <Statistics
-            good={this.state.good}
-            neutral={this.state.neutral}
-            bad={this.state.bad}
-            total={this.countTotalFeedback()}
-            positivePercentage={this.countPositiveFeedbackPercentage()}
-          />
-        ) : (
-          <Notification message="There is no feedback"></Notification>
-        )}
-      </Section>
-    );
-  }
+  const options = [
+    { id: 'id-1', label: 'Good', bgc: '#4de876' },
+    { id: 'id-2', label: 'Neutral', bgc: '#f0ee6c' },
+    { id: 'id-3', label: 'Bad', bgc: '#f03c3c' },
+  ];
+  return (
+    <Section title="Please leave feedback">
+      <FeedbackOptions
+        options={options}
+        onLeaveFeedback={onLeaveFeedback}
+      ></FeedbackOptions>
+      {countTotalFeedback() > 0 ? (
+        <Statistics
+          good={good}
+          neutral={neutral}
+          bad={bad}
+          total={countTotalFeedback()}
+          positivePercentage={countPositiveFeedbackPercentage()}
+        />
+      ) : (
+        <Notification message="There is no feedback"></Notification>
+      )}
+    </Section>
+  );
 }
--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -5,12 +5,17 @@ import styles from './FeedbackOptions.module.css';
 export default class FeedbackOptions extends Component {
   render() {
     const { options, onLeaveFeedback } = this.props;
+    // const options = ['good', 'neutral', 'bad'];
     return (
       <ul className={styles.options__list}>
-        {options.map((option, index) => (
-          <li key={index} className={styles.options__item}>
-            <button className={styles.options__btn} onClick={onLeaveFeedback}>
-              {option}
+        {options.map(option => (
+          <li key={option.id} className={styles.options__item}>
+            <button
+              className={styles.options__btn}
+              onClick={onLeaveFeedback}
+              style={{ backgroundColor: option.bgc }}
+            >
+              {option.label.toLowerCase()}
             </button>
           </li>
         ))}
@@ -20,6 +25,12 @@ export default class FeedbackOptions extends Component {
 }
 
 FeedbackOptions.propTypes = {
-  options: PropTypes.arrayOf(PropTypes.string.isRequired),
+  options: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      label: PropTypes.string.isRequired,
+      bgc: PropTypes.string.isRequired,
+    })
+  ),
   onLeaveFeedback: PropTypes.func.isRequired,
 };
